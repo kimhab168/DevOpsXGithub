489a2d4a6c8ec1e383874b58f0227f813c4ff1cf
MyDragon168 <cambodiagames3@gmail.com>
2024-08-21 13:07:27 +0700

    CICD

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../app/components/**/*.stories.tsx",
+    "../app/components/*.stories.tsx",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "msw-storybook-addon",
+    "@storybook/addon-coverage",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"], // Use forward slashes for cross-platform compatibility
+};
+
+export default config;
